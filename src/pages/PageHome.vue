d34c1240f97ebc4870ddca6ee1d98d27f197cd4b
Hammad-khalil <shhamadkhalil0786@gmail.com>
2024-01-12 19:36:27 +0500

    Functionality Added So Tweet Button Scrolls down when text moves on Next Line

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,35 +1,36 @@
 <template>
   <q-page>
-    <div class="q-py-lg q-px-md">
-      <q-input
-        class="newTweet"
-        filled
-        bottom-slots
-        v-model="totalchar"
-        placeholder="What's Happening ?"
-        maxlength="280"
-        counter
-        autogrow
-      >
-        <template v-slot:before>
-          <q-avatar size="xl">
-            <img
-              src="https://avatars.githubusercontent.com/u/143031265?s=400&u=94d880598a0efaa8d66727251d8c9d0a058e0be1&v=4"
-            />
-          </q-avatar>
-        </template>
-
-        <template v-slot:after>
-          <q-btn
-            :disable="!totalchar"
-            unelevated
-            rounded
-            color="primary"
-            label="Tweet"
-            no-caps
-          />
-        </template>
-      </q-input>
+    <div class="q-py-lg q-px-md row items-end q-gutter-sm">
+      <div class="col">
+        <q-input
+          class="newTweet"
+          filled
+          bottom-slots
+          v-model="totalchar"
+          placeholder="What's Happening ?"
+          maxlength="280"
+          counter
+          autogrow
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img
+                src="https://avatars.githubusercontent.com/u/143031265?s=400&u=94d880598a0efaa8d66727251d8c9d0a058e0be1&v=4"
+              />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          :disable="!totalchar"
+          unelevated
+          rounded
+          color="primary"
+          label="Tweet"
+          no-caps
+        />
+      </div>
     </div>
   </q-page>
 </template>
